f42d00e63741c5f28997c9ce9a3759ee888cf8d6
András Szesztai <andras.szesztai@nielsen.com>
2022-05-22 15:27:36 +0200

    style: install emotion, add designTokens

--- components/molecules/SummaryChart/SummaryChart.tsx
+++ components/molecules/SummaryChart/SummaryChart.tsx
@@ -1,7 +1,16 @@
+import styled from '@emotion/styled'
+
+import { designTokens } from 'styles/designTokens'
 import { Props } from './types'
 
+const Container = styled.div`
+    width: ${designTokens.space.xl}px;
+    height: ${designTokens.space.xl}px;
+    background-color: red;
+`
+
 const SummaryChart = ({ data }: Props) => {
-  return <div>{data.overall}</div>
+    return <Container>{data.overall}</Container>
 }
 
 export default SummaryChart
---
+++ styles/designTokens.ts
@@ -0,0 +1,15 @@
+export const designTokens = {
+    space: {
+        xxs: 4,
+        xs: 8,
+        sm: 12,
+        base: 16,
+        md: 24,
+        lg: 32,
+        xl: 48,
+        xxl: 64,
+        '3xl': 96,
+        '4xl': 128,
+        '5xl': 192,
+    },
+}
